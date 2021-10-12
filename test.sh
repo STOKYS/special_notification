011823952c394df4e6c5763901ceedd9f58602eb
Filip <47699264+SeezoCode@users.noreply.github.com>
2021-10-12 17:10:52 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
-arr=("Postavil jsem sve dceri barak" "Doufam ze umis Cisco" "Sleduji te" "Dneska budeme delat Dockery" "Tak si trosku zatancime" "Sed rovne!" "Ukazeme si routovani")
+arr=("Postavil jsem sve dceri barak" "Doufam ze umis Cisco" "Sleduji te" "Dneska budeme delat Dockery" "Tak si trosku zatancime" "Sed rovne!" "Ukazeme si routovani" "Postavil jsem sve dceri barak, co ty?" "Černí smrdí jinak" "Byl jsem v Americe a Singapuru" "Choď rovně!" "Big Blue Button" "sleduji te :)" "Jsi na správném operačním systému")
 min=0
-max=7
+max=14
 while true
 do
     number=$(expr $min + $RANDOM % $max)
     notify-send "Petr Grussmann" "${arr[$number]}" --icon="$HOME/gruss.jpg"
-    sleep 10s
+    sleep 1500s
 done
